f10448b42943c0d7c63977609a4f9f3f5c94d022
Habilya <andrei@prdcrazyserver>
2018-10-26 16:00:50 -0400

    Adjust top 3 process display.

--- SCRIPT/MOTD.sh
+++ SCRIPT/MOTD.sh
@@ -23,25 +23,33 @@ ADMINS=`cat /etc/group | grep --regex "^sudo" | awk -F: '{print $4}' | tr ',' '|
 ADMINSLIST=`grep -E $ADMINS /etc/passwd | tr ':' ' ' | tr ',' ' ' | awk {'print $5,$6,"("$1")"'} | tr '\n' ',' | sed '$s/.$//'`
 UPDATESAVAIL=`cat /var/zzscriptzz/MOTD/updates-available.dat`
 
-cur_temperature=$(cat /sys/class/thermal/thermal_zone0/temp)
-cur_temperature=$(echo "$cur_temperature/1000" | bc -l | xargs printf "%1.0f")
+if [ -f /sys/class/thermal/thermal_zone0/temp ]; then
+        cur_temperature=$(cat /sys/class/thermal/thermal_zone0/temp)
+        cur_temperature="$(echo "$cur_temperature/1000" | bc -l | xargs printf "%1.0f")°C"
+else
+        cur_temperature="N/A"
+fi
+
+OPEN_PORTS_IPV4=`netstat -lnt | awk 'NR>2{print $4}' | grep -E '0.0.0.0:' | sed 's/.*://' | sort -n | uniq | awk -vORS=, '{ print $1 }' | sed 's/,$/\n/'`
+OPEN_PORTS_IPV6=`netstat -lnt | awk 'NR>2{print $4}' | grep -E ':::' | sed 's/.*://' | sort -n | uniq | awk -vORS=, '{ print $1 }' | sed 's/,$/\n/'`
 
 ps_output="$(ps aux)"
 processes="$(printf "%s\\n" "${ps_output}" | wc -l)"
-mem_top_processes="$(printf "%s\\n" "${ps_output}" | awk '{print $2, $4"%", $11}' | sort -k2rn | head -3 | awk '{print "\t\tID: "$1, $3, $2}')"
-cpu_top_processes="$(printf "%s\\n" "${ps_output}" | awk '{print $2, $3"%", $11}' | sort -k2rn | head -3 | awk '{print "\t\tID: "$1, $3, $2}')"
+mem_top_processes="$(printf "%s\\n" "${ps_output}" | awk '{print "\033[1;37m"$2, $4"%", "\033[1;32m"$11}' | sort -k2rn | head -3 | awk '{print " \033[0;35m+\t\033[1;32mID: "$1, $3, $2}')"
+cpu_top_processes="$(printf "%s\\n" "${ps_output}" | awk '{print "\033[1;37m"$2, $3"%", "\033[1;32m"$11}' | sort -k2rn | head -3 | awk '{print " \033[0;35m+\t\033[1;32mID: "$1, $3, $2}')"
+
 
 # get the load averages
 read one five fifteen rest < /proc/loadavg
 
-if [[ $GROUPZ == "$USER sudo" ]]; then
-USERGROUP="Administrator"
-elif [[ $USER = "root" ]]; then
-USERGROUP="Root"
-elif [[ $USER = "$USER" ]]; then
-USERGROUP="Regular User"
+if [[ "$GROUPZ" == *"sudo"* ]]; then
+        USERGROUP="Administrator"
+elif [[ "$USER" == "root" ]]; then
+        USERGROUP="Root"
+elif [[ "$USER" == "$USER" ]]; then
+        USERGROUP="Regular User"
 else
-USERGROUP="$GROUPZ"
+        USERGROUP="$GROUPZ"
 fi
 
 echo -e " ${C0}+                    +                     +         +
@@ -58,31 +66,34 @@ echo -e " ${C0}+                    +                     +         +
     |${C6}:.${C3}| ${C6}.${C3}|   |  | | |${C6}:.:${C3}|| ${C6}.${C3} |  |${C6}==${C3}| |     |${C6}=${C3}|${C6}===${C3}|${C6} .${C3}   |    | |   |
     |     |      |   |   |            :   .   |   ;     ;          |
           :          :                .          .      .          :
-    ${C2}
-
 "
 
-echo -e " ${C1} ++++++++++++++++++++++++: ${C3}System Data${C1} :+++++++++++++++++++++++++++
+echo -e "${C1} ++++++++++++++++++++++++: ${C3}System Data${C1} :+++++++++++++++++++++++++++
 ${C1} + ${C3}Hostname       ${C1}=  ${C4}`hostname` ${C0}(`hostname --fqdn`)
 ${C1} + ${C3}IPv4 Address   ${C1}=  ${C4}`wget http://ipinfo.io/ip -qO -` ${C0}(`ip addr list eth0 |grep "inet " |cut -d' ' -f6|cut -d/ -f1`)
 ${C1} + ${C3}Uptime         ${C1}=  ${C4}`uptime | sed -E 's/^[^,]*up *//; s/, *[[:digit:]]* users.*//; s/min/minutes/; s/([[:digit:]]+):0?([[:digit:]]+)/\1 hours, \2 minutes/'`
 ${C1} + ${C3}Time           ${C1}=  ${C0}`date`
-${C1} + ${C3}CPU T°         ${C1}=  ${C0}$cur_temperature°C
+${C1} + ${C3}CPU T°         ${C1}=  ${C0}$cur_temperature
+${C1} + ${C3}Processes      ${C1}=  ${C4}$PROCCOUNT of `ulimit -u` max
 ${C1} + ${C3}Load Averages  ${C1}=  ${C4}${one}, ${five}, ${fifteen} ${C0}(1, 5, 15 min)
 ${C1} + ${C3}Distro         ${C1}=  ${C4}`cat /etc/*release | grep "PRETTY_NAME" | cut -d "=" -f 2- | sed 's/"//g'` ${C0}(`uname -r`)
 ${C1} + ${C3}CPU            ${C1}=  ${C4}`cat /proc/cpuinfo | grep "model name" | cut -d ' ' -f3- | awk {'print $1,$2,$3,$4,$5,$6,$7,$8,$9,$10'} | head -1`
 ${C1} + ${C3}Memory         ${C1}=  ${C4}`free -m | awk '/Mem/ { printf("%3.1f%%", $3/$2*100) }'` ${C0}(`free -t -m | grep "Mem" | awk {'print $4'}`MB Free, `free -t -m | grep "Mem" | awk {'print $3'}`MB/`free -t -m | grep "Mem" | awk {'print $2'}`MB Used)
 ${C1} + ${C3}Swap           ${C1}=  ${C4}`free -m | awk '/Swap/ { printf("%3.1f%%", $3/$2*100) }'` ${C0}(`free -t -m | grep "Swap" | awk {'print $4'}`MB Free, `free -t -m | grep "Swap" | awk {'print $3'}`MB/`free -t -m | grep "Swap" | awk {'print $2'}`MB Used)
-${C1} + ${C3}HDD Usage      ${C1}=  ${C4}`df -H | grep "/dev/root" | awk '{ print $5 }'` ${C0}(`df -hT | grep "/dev/root" | awk {'print $5'}`B Free, `df -hT | grep "/dev/root" | awk {'print $4'}`B/`df -hT | grep "/dev/root" | awk {'print $3'}`B Used)
+${C1} + ${C3}HDD Usage      ${C1}=  ${C4}`df -H | grep "/dev/.*root" | awk '{ print $5 }'` ${C0}(`df -hT | grep "/dev/.*root" | awk {'print $5'}`B Free, `df -hT | grep "/dev/.*root" | awk {'print $4'}`B/`df -hT | grep "/dev/.*root" | awk {'print $3'}`B Used)
 ${C1} + ${C3}Updates        ${C1}=  ${C4}$UPDATESAVAIL ${C0}"Updates Available"
+${C1} ++++++++++++++++++++: ${C3}Top CPU Processes${C1} :+++++++++++++++++++++++++
+$cpu_top_processes${C0}
+${C1} ++++++++++++++++++++: ${C3}Top Mem Processes${C1} :+++++++++++++++++++++++++
+$mem_top_processes${C0}
 ${C1} ++++++++++++++++++++++++: ${C3}User Data${C1} :+++++++++++++++++++++++++++++
 ${C1} + ${C3}Username       ${C1}=  ${C4}`whoami` ${C0}($USERGROUP)
 ${C1} + ${C3}Last Login     ${C1}=  ${C4}`last -a $USER | head -2 | awk 'NR==2{print $3,$4,$5,$6}'` from `last -a $USER | head -2 | awk 'NR==2{print $10}'`
 ${C1} + ${C3}Sessions       ${C1}=  ${C4}`who | grep $USER | wc -l`
-${C1} + ${C3}Processes      ${C1}=  ${C4}$PROCCOUNT of `ulimit -u` max
-${C1} + ${C3}Top Mem Processes${C1}: \n${C4}$mem_top_processes${C0}
-${C1} + ${C3}Top CPU Processes${C1}: \n${C4}$cpu_top_processes${C0}
 ${C1} ++++++++++++++++++++: ${C3}Helpful Information${C1} :+++++++++++++++++++++++
 ${C1} + ${C3}Administrators ${C1}=  ${C4}$ADMINSLIST
+${C1} + ${C3}OpenPorts IPv4 ${C1}=  ${C4}$OPEN_PORTS_IPV4
+${C1} + ${C3}OpenPorts IPv6 ${C1}=  ${C4}$OPEN_PORTS_IPV6
 ${C1} ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
 "
+
