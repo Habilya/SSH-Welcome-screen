db58ecdb5d752c025514e047142f2c49a464bfae
Habilya <andrei@prdcrazyserver>
2019-10-02 17:23:13 -0400

    Added more documentation

--- SCRIPT/MOTD.sh
+++ SCRIPT/MOTD.sh
@@ -72,31 +72,36 @@ processes="$(printf "%s\\n" "${ps_output}" | wc -l)"
 mem_top_processes="$(printf "%s\\n" "${ps_output}" | awk '{print "\033[1;37m"$2, $4"%", "\033[1;32m"$11}' | sort -k2rn | head -3 | awk '{print " \033[0;35m+\t\033[1;32mID: "$1, $3, $2}')"
 cpu_top_processes="$(printf "%s\\n" "${ps_output}" | awk '{print "\033[1;37m"$2, $3"%", "\033[1;32m"$11}' | sort -k2rn | head -3 | awk '{print " \033[0;35m+\t\033[1;32mID: "$1, $3, $2}')"
 
+# Get your remote IP address using external resource ipinfo.io
 remote_ip="$(wget http://ipinfo.io/ip -qO -)"
+# Get your local IP address
 local_ip="$(ip addr list $INTERFACE | grep "inet " | cut -d' ' -f6| cut -d/ -f1)"
+# Get the total machine uptime in specific dynamic format 0 days, 0 hours, 0 minutes
 machine_uptime="$(uptime | sed -E 's/^[^,]*up *//; s/, *[[:digit:]]* user.*//; s/min/minutes/; s/([[:digit:]]+):0?([[:digit:]]+)/\1 hours, \2 minutes/')"
+# Get your linux distro name
 distro_pretty_name="$(grep "PRETTY_NAME" /etc/*release | cut -d "=" -f 2- | sed 's/"//g')"
+# Get the brand and model of your CPU
 cpu_model_name="$(grep "model name" /proc/cpuinfo | cut -d ' ' -f3- | awk '{print $1,$2,$3,$4,$5,$6,$7,$8,$9,$10}' | head -1)"
 
-#memory usage
+# Get memory usage to be displayed
 memory_percent="$(free -m | awk '/Mem/ { if($2 ~ /^[1-9]+/) memm=$3/$2*100; else memm=0; printf("%3.1f%%", memm) }')"
 memory_free_mb="$(free -t -m | grep "Mem" | awk '{print $4}')"
 memory_used_mb="$(free -t -m | grep "Mem" | awk '{print $3}')"
 memory_available_mb="$(free -t -m | grep "Mem" | awk '{print $2}')"
 
-#swap usage
+# Get SWAP usage to be displayed
 swap_percent="$(free -m | awk '/Swap/ { if($2 ~ /^[1-9]+/) swapm=$3/$2*100; else swapm=0; printf("%3.1f%%", swapm) }')"
 swap_free_mb="$(free -t -m | grep "Swap" | awk '{print $4}')"
 swap_used_mb="$(free -t -m | grep "Swap" | awk '{print $3}')"
 swap_available_mb="$(free -t -m | grep "Swap" | awk '{print $2}')"
 
-#HDD usage
+# Get HDD usage to be displayed
 hdd_percent="$(df -H | grep "/$" | awk '{ print $5 }')"
 hdd_free="$(df -hT | grep "/$" | awk '{print $5}')"
 hdd_used="$(df -hT | grep "/$" | awk '{print $4}')"
 hdd_available="$(df -hT | grep "/$" | awk '{print $3}')"
 
-#lastlogin
+#Get last login information (user, ip)
 last_login_user="$(last -a $USER | head -2 | awk 'NR==2{print $3,$4,$5,$6}')"
 last_login_ip="$(last -a $USER | head -2 | awk 'NR==2{print $10}')"
 
@@ -118,6 +123,7 @@ fi
 clear && printf '\e[3J'
 
 # Print a city scape (purely aesthetic)
+# If "you no like", delete it or replace with your own ;)
 echo -e " ${C0}+                    +                     +         +
                                  +                  +           +
           +                                             +
